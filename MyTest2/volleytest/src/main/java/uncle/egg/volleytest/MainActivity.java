1d1b1487b07dae44e4b9438c1b126f84bae068ac
EggUncle <cicadashadow@gmail.com>
2016-07-18 23:48:29 +0800

    20160718
    关于Volley框架的网络图片获取

--- MyTest2/volleytest/src/main/java/uncle/egg/volleytest/MainActivity.java
+++ MyTest2/volleytest/src/main/java/uncle/egg/volleytest/MainActivity.java
@@ -1,16 +1,21 @@
 package uncle.egg.volleytest;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,6 +28,7 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     private TextView text;
+    private ImageView img;
 
 
     @Override
@@ -30,7 +36,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.text);
-        volley_Get();
+        img = (ImageView) findViewById(R.id.img);
+        //     volley_Get();
+        getImage();
+    }
+
+
+    private void getImage() {
+        String url = "http://www.owbase.com/uploads/newspic/images/2016/2016062501.jpg";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        //ImageRequest的构造函数接收六个参数，
+        // 第一个参数就是图片的URL地址
+        // 第二个参数是图片请求成功的回调，这里我们把返回的Bitmap参数设置到ImageView中
+        // 第三第四个参数分别用于指定允许图片最大的宽度和高度，
+        // 如果指定的网络图片的宽度或高度大于这里的最大值，则会对图片进行压缩，
+        // 指定成0的话就表示不管图片有多大，都不会进行压缩
+        // 。第五个参数用于指定图片的颜色属性，
+        // Bitmap.Config下的几个常量都可以在这里使用，其中ARGB_8888可以展示最好的颜色属性
+        // ，每个图片像素占据4个字节的大小，而RGB_565则表示每个图片像素占据2个字节大小。
+        // 第六个参数是图片请求失败的回调，
+        ImageRequest imageRequest = new ImageRequest(url, new Response.Listener<Bitmap>() {
+
+            @Override
+            public void onResponse(Bitmap bitmap) {
+                img.setImageBitmap(bitmap);
+            }
+        }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                img.setImageResource(R.mipmap.ic_launcher);
+            }
+        });
+        imageRequest.setTag("imgGet");
+        MyApplication.getHttpQueues().add(imageRequest);
     }
 
     private void volley_Get() {
@@ -61,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                         JSONArray catName;
                         try {
                             catName = jsonObject.getJSONArray("results");
-                            String s=catName.optJSONObject(1).get("title").toString();
+                            String s = catName.optJSONObject(1).get("title").toString();
                             String tel = jsonObject.optString("title");
                             text.setText(s + " " + tel);
                         } catch (JSONException e) {
@@ -74,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
 
-                             text.setText(volleyError.toString());
+                        text.setText(volleyError.toString());
                     }
                 });
 
