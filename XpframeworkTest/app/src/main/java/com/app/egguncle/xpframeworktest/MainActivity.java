4f8cc96980d7c2cfd0dc8816b79268d4789137e6
egg_uncle <cicadashadow@gmail.com>
2016-11-28 20:42:50 +0800

    XP框架

---
+++ XpframeworkTest/app/src/main/java/com/app/egguncle/xpframeworktest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.app.egguncle.xpframeworktest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * 关于XP框架的尝试，hook掉整个onclick方法
+ */
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MY_TAG","click");
+            }
+        });
+    }
+}
---
+++ XpframeworkTest/myxposedmodule/src/main/java/com/app/egguncle/myxposedmodule/XpUtil.java
@@ -0,0 +1,32 @@
+package com.app.egguncle.myxposedmodule;
+
+import android.util.Log;
+import android.view.View;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by egguncle on 16.11.28.
+ */
+
+public class XpUtil implements IXposedHookLoadPackage{
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if(!lpparam.packageName.equals("com.app.egguncle.xpframeworktest"))
+            return;
+
+        XposedBridge.log("Loaded app:"+lpparam.packageName);
+
+        XposedHelpers.findAndHookMethod("com.app.egguncle.xpframeworktest.MainActivity", lpparam.classLoader, "onClick", View.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                XposedBridge.log("HOOK SUCCESS");
+            }
+        });
+    }
+}
